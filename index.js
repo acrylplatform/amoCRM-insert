ec3998c57a1e6c9fdab3c07dac72793d74b7e532
vseriousv <gavrilovskirill@gmail.com>
2019-10-15 14:51:54 +0700

    Refactoring Code

--- index.js
+++ index.js
@@ -20,107 +20,98 @@ const crm = new AmoCRM({
     }
 });
 
-const addCRM = data => {
+const addCRM = async (data) => {
     //Search TxAssetID in CRM 
-    console.log(data.TxAssetID, ": CRM search");
-    crm.Lead.find({query: data.TxAssetID})
-    .then((res) => {
-        
-        if ( res.length === 0 ){
-            console.log(data.TxAssetID, ": Add to the CRM")
-            //add contact
-            crm.Contact.insert([{
-                name: data.customName,
-                responsible_user_id: "3316417",
-                tags: "купить майнер",
+    await console.log(data.TxAssetID, ": CRM search");
+    let queryRes1 = await crm.Lead.find({query: data.TxAssetID});
+    let queryRes2 = await crm.Lead.find({query: data.TxAssetID});
+    
+    if ( queryRes1.length === 0 && queryRes2.length === 0 ){
+        await console.log(data.TxAssetID, ": Add to the CRM");
+        //add contact
+        await crm.Contact.insert([{
+            name: data.customName,
+            responsible_user_id: "3316417",
+            tags: "купить майнер",
+            custom_fields: [
+                { 
+                    id: "543953",
+                    name: 'Телефон',
+                    code: 'PHONE',
+                    values: [{
+                        value: data.phone,
+                        enum: 907777
+                    }],
+                    is_system: true 
+                },
+                { 
+                    id: "543955",
+                    name: 'Email',
+                    code: 'EMAIL',
+                    values: [{
+                        value: data.email,
+                        enum: 907789
+                    }],
+                    is_system: true 
+                }
+            ]
+        }])
+        .then(result => {
+            //add lead
+            crm.Lead.insert([{
+                name: 'Покупка майнеров с client`а',
+                status_id: "29857402",
+                responsible_user_id: "55014691",
+                sale: 115,
+                tags: "buy_miner",
+                contacts_id: [
+                    result._response._embedded.items[0].id 
+                ],
                 custom_fields: [
-                    { 
-                        id: "543953",
-                        name: 'Телефон',
-                        code: 'PHONE',
-                        values: [{
-                            value: data.phone,
-                            enum: 907777
+                    {
+                        id: 657873,
+                        name: "Адрес доставки",
+                        values: [{ 
+                            value: data.postCode + ', ' + data.country + ', ' + data.countryState + ', ' + data.city + ', ' + data.address, 
                         }],
-                        is_system: true 
+                        is_system: false 
                     },
-                    { 
-                        id: "543955",
-                        name: 'Email',
-                        code: 'EMAIL',
-                        values: [{
-                            value: data.email,
-                            enum: 907789
+                    {
+                        id: 664225,
+                        name: "TxAssetID",
+                        values: [{ 
+                            value: data.TxAssetID
                         }],
-                        is_system: true 
-                    }
+                        is_system: false
+                    },
+                    {
+                        id: 664227,
+                        name: "countMiners",
+                        values: [{ 
+                            value: data.countMiners
+                        }],
+                        is_system: false
+                    },
+                    {
+                        id: 664381,
+                        name: "referal",
+                        values: [{ 
+                            value: data.referal
+                        }],
+                        is_system: false
+                    },
                 ]
             }])
-            .then(result => {
-                //add lead
-                crm.Lead.insert([{
-                    name: 'Покупка майнеров с client`а',
-                    status_id: "29857402",
-                    responsible_user_id: "55014691",
-                    sale: 115,
-                    tags: "buy_miner",
-                    contacts_id: [
-                        result._response._embedded.items[0].id 
-                    ],
-                    custom_fields: [
-                        {
-                            id: 657873,
-                            name: "Адрес доставки",
-                            values: [{ 
-                                value: data.postCode + ', ' + data.country + ', ' + data.countryState + ', ' + data.city + ', ' + data.address, 
-                            }],
-                            is_system: false 
-                        },
-                        {
-                            id: 664225,
-                            name: "TxAssetID",
-                            values: [{ 
-                                value: data.TxAssetID
-                            }],
-                            is_system: false
-                        },
-                        {
-                            id: 664227,
-                            name: "countMiners",
-                            values: [{ 
-                                value: data.countMiners
-                            }],
-                            is_system: false
-                        },
-                        {
-                            id: 664381,
-                            name: "referal",
-                            values: [{ 
-                                value: data.referal
-                            }],
-                            is_system: false
-                        },
-                    ]
-                }])
-                .catch(error => console.error('Error with add lead: ', error));;
-            })
-            .catch(error => console.error('Error with add contact: ', error));
-        }else{
-            console.log(data.TxAssetID, ': is in the CRM');
-        }
-    })
+            .catch(error => console.error('Error with add lead: ', error));;
+        })
+        .catch(error => console.error('Error with add contact: ', error));
+    }else{
+        console.log(data.TxAssetID, ': is in the CRM');
+    }
 }
 
 exports.handler = (event, context, callback) => {
-    // crm.connect().then(() => {
-    // console.log( `Sign in portal` );
-
         getDataDynamoDB.then(result => {   
             result.map(data => addCRM(data));
-        });
-
-    // })
-    // .catch( error => {
-    //     console.log( 'Error login: ', error );
-    // });  
+        }); 
 }
