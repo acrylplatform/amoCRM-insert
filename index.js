2f957536de963374c4faf2e5614971be9312d501
vseriousv <gavrilovskirill@gmail.com>
2019-10-31 11:54:59 +0700

    Refactoring code

--- index.js
+++ index.js
@@ -20,99 +20,129 @@ const crm = new AmoCRM({
     }
 });
 
-const addCRM = async (data) => {
-    //Search TxAssetID in CRM 
-    await console.log(data.TxAssetID, ": CRM search");
+var diff = function (arr1, arr2) {
+    
+    arr2.map(function (item2) {
+        arr1 = arr1.filter(function (item1) {
+            return item2 != item1.TxAssetID
+        });
+    });
+    return arr1;
+};
 
-    let lead_result = await crm.Lead.find({"query":data.TxAssetID})
-    let lead_bool = await lead_result[0] != undefined ? true : false;
-  
-    if ( !lead_bool ){
-        await console.log(data.TxAssetID, ": Add to the CRM");
-        //add contact
-        await crm.Contact.insert([{
-            name: data.customName,
-            responsible_user_id: "3316417",
-            tags: "купить майнер",
+const addCRM = async (data) => {
+    await console.log(data.TxAssetID, ": Add to the CRM");
+    //add contact
+    await crm.Contact.insert([{
+        name: data.customName,
+        responsible_user_id: "3316417",
+        tags: "купить майнер",
+        custom_fields: [
+            { 
+                id: "543953",
+                name: 'Телефон',
+                code: 'PHONE',
+                values: [{
+                    value: data.phone,
+                    enum: 907777
+                }],
+                is_system: true 
+            },
+            { 
+                id: "543955",
+                name: 'Email',
+                code: 'EMAIL',
+                values: [{
+                    value: data.email,
+                    enum: 907789
+                }],
+                is_system: true 
+            }
+        ]
+    }])
+    .then(result => {
+        //add lead
+        crm.Lead.insert([{
+            name: 'Покупка майнеров с client`а',
+            status_id: "29857402",
+            responsible_user_id: "55014691",
+            sale: 115,
+            tags: "buy_miner",
+            contacts_id: [
+                result._response._embedded.items[0].id 
+            ],
             custom_fields: [
-                { 
-                    id: "543953",
-                    name: 'Телефон',
-                    code: 'PHONE',
-                    values: [{
-                        value: data.phone,
-                        enum: 907777
+                {
+                    id: 657873,
+                    name: "Адрес доставки",
+                    values: [{ 
+                        value: data.postCode + ', ' + data.country + ', ' + data.countryState + ', ' + data.city + ', ' + data.address, 
                     }],
-                    is_system: true 
+                    is_system: false 
                 },
-                { 
-                    id: "543955",
-                    name: 'Email',
-                    code: 'EMAIL',
-                    values: [{
-                        value: data.email,
-                        enum: 907789
+                {
+                    id: 664225,
+                    name: "TxAssetID",
+                    values: [{ 
+                        value: data.TxAssetID
                     }],
-                    is_system: true 
-                }
+                    is_system: false
+                },
+                {
+                    id: 664227,
+                    name: "countMiners",
+                    values: [{ 
+                        value: data.countMiners
+                    }],
+                    is_system: false
+                },
+                {
+                    id: 664381,
+                    name: "referal",
+                    values: [{ 
+                        value: data.referal
+                    }],
+                    is_system: false
+                },
             ]
         }])
-        .then(result => {
-            //add lead
-            crm.Lead.insert([{
-                name: 'Покупка майнеров с client`а',
-                status_id: "29857402",
-                responsible_user_id: "55014691",
-                sale: 115,
-                tags: "buy_miner",
-                contacts_id: [
-                    result._response._embedded.items[0].id 
-                ],
-                custom_fields: [
-                    {
-                        id: 657873,
-                        name: "Адрес доставки",
-                        values: [{ 
-                            value: data.postCode + ', ' + data.country + ', ' + data.countryState + ', ' + data.city + ', ' + data.address, 
-                        }],
-                        is_system: false 
-                    },
-                    {
-                        id: 664225,
-                        name: "TxAssetID",
-                        values: [{ 
-                            value: data.TxAssetID
-                        }],
-                        is_system: false
-                    },
-                    {
-                        id: 664227,
-                        name: "countMiners",
-                        values: [{ 
-                            value: data.countMiners
-                        }],
-                        is_system: false
-                    },
-                    {
-                        id: 664381,
-                        name: "referal",
-                        values: [{ 
-                            value: data.referal
-                        }],
-                        is_system: false
-                    },
-                ]
-            }])
-            .catch(error => console.error('Error with add lead: ', error));;
-        })
-        .catch(error => console.error('Error with add contact: ', error));
-    }else{
-        console.log(data.TxAssetID, ': is in the CRM');
-    }
+        .catch(error => console.error('Error with add lead: ', error));;
+    })
+    .catch(error => console.error('Error with add contact: ', error));
 }
 
 exports.handler = (event, context, callback) => {
-        getDataDynamoDB.then(result => {   
-            result.map(data => addCRM(data));
-        }); 
+    let DataArrayDynamoDB= new Array;
+    let DataArrayCRM= new Array;
+
+    //get to DB    
+    const DataDynamoDB = await getDataDynamoDB;
+    await DataDynamoDB.map(data => { DataArrayDynamoDB.push(data.TxAssetID) });
+    await console.log("DataArrayDynamoDB",DataArrayDynamoDB);
+  
+    //get to CRM
+    let lead_result = await crm.Lead.find({"query":"buy_miner"});
+    await lead_result.map(lead => { DataArrayCRM.push(lead.attributes.custom_fields[1].values[0].value) });
+    await console.log("DataArrayCRM:", DataArrayCRM);
+
+    if(DataArrayCRM.length == 0){
+        //Make strict zero length, becouse in CRM have leads!
+        while (DataArrayCRM.length == 0){
+            //get to CRM
+            let lead_result = await crm.Lead.find({"query":"buy_miner"}); 
+            await lead_result.map(lead => { DataArrayCRM.push(lead.attributes.custom_fields[1].values[0].value) });
+            await console.log("DataArrayCRM_double:", DataArrayCRM);
+        }
+    }
+
+    let afterArray = await diff(DataDynamoDB, DataArrayCRM);
+
+    await console.log("afterArray: ", afterArray.length);
+
+    if(afterArray.length == 0){
+        await console.log("is in the CRM");
+    }else{
+        // await console.log("is not in the CRM");
+        await afterArray.map(data => addCRM(data))
+    }
 }
