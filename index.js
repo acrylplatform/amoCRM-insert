719995ac16cd31df1f42fe396f6af4a0052606de
vseriousv <gavrilovskirill@gmail.com>
2019-11-02 00:22:47 +0700

    Checked function for DynamoDB Trigger

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const AmoCRM = require( 'amocrm-js' );
 var AWS = require("aws-sdk");
-require('dotenv').config();
 
 AWS.config.update({region: 'eu-central-1'});
-
-var dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-//Get array with objects from database
-const getDataDynamoDB = new Promise((resolve, reject) => {
-    dynamoDb.scan({TableName: process.env.TableName}).promise()
-    .then(data => resolve(data.Items));
-});
 
 const crm = new AmoCRM({
     domain: process.env.domain, 
@@ -19,134 +10,95 @@ const crm = new AmoCRM({
         hash: process.env.auth_hash
     }
 });
-const addCRM = async (data) => {
 
-    //add contact
-    let contact = await crm.Contact.insert([{
-        name: data.customName,
-        responsible_user_id: "3316417",
-        tags: "купить майнер",
-        custom_fields: [
-            { 
-                id: "543953",
-                name: 'Телефон',
-                code: 'PHONE',
-                values: [{
-                    value: data.phone,
-                    enum: 907777
-                }],
-                is_system: true 
-            },
-            { 
-                id: "543955",
-                name: 'Email',
-                code: 'EMAIL',
-                values: [{
-                    value: data.email,
-                    enum: 907789
-                }],
-                is_system: true 
-            }
-        ]
-    }]);
-
-    await console.log("Contanct ID", contact._response._embedded.items[0].id );
-
-    await console.log(data.TxAssetID, ": Add to Lead");
-
-    await crm.Lead.insert([{
-        name: 'Покупка майнеров с client`а',
-        status_id: "29857402",
-        responsible_user_id: "55014691",
-        sale: 115,
-        tags: "buy_miner",
-        contacts_id: [
-            contact._response._embedded.items[0].id 
-        ],
-        custom_fields: [
-            {
-                id: 657873,
-                name: "Адрес доставки",
-                values: [{ 
-                    value: data.postCode + ', ' + data.country + ', ' + data.countryState + ', ' + data.city + ', ' + data.address, 
-                }],
-                is_system: false 
-            },
-            {
-                id: 664225,
-                name: "TxAssetID",
-                values: [{ 
-                    value: data.TxAssetID
-                }],
-                is_system: false
-            },
-            {
-                id: 664227,
-                name: "countMiners",
-                values: [{ 
-                    value: data.countMiners
-                }],
-                is_system: false
-            },
-            {
-                id: 664381,
-                name: "referal",
-                values: [{ 
-                    value: data.referal
-                }],
-                is_system: false
-            },
-        ]
-    }]);
-
-    await console.log("added lead")
-}
-
-var diff = function (arr1, arr2) {
-    
-    arr2.map(function (item2) {
-        arr1 = arr1.filter(function (item1) {
-            return item2 != item1.TxAssetID
-        });
-    });
-    return arr1;
-};
-
-const foo = async () => {
-    let DataArrayDynamoDB= new Array;
-    let DataArrayCRM= new Array;
-
-    //get to DB    
-    const DataDynamoDB = await getDataDynamoDB;
-    await DataDynamoDB.map(data => { DataArrayDynamoDB.push(data.TxAssetID) });
-    // await console.log("DataArrayDynamoDB",DataArrayDynamoDB);
-  
-    //get to CRM
-    let lead_result = await crm.Lead.find({"query":"buy_miner"});
-    await lead_result.map(lead => { DataArrayCRM.push(lead.attributes.custom_fields[1].values[0].value) });
-    // await console.log("DataArrayCRM:", DataArrayCRM);
-
-    if(DataArrayCRM.length == 0){
-        //Make strict zero length, becouse in CRM have leads!
-        while (DataArrayCRM.length == 0){
-            //get to CRM
-            let lead_result = await crm.Lead.find({"query":"buy_miner"}); 
-            await lead_result.map(lead => { DataArrayCRM.push(lead.attributes.custom_fields[1].values[0].value) });
-            // await console.log("DataArrayCRM_double:", DataArrayCRM);
-        }
-    }
-
-    let diffArray = await diff(DataDynamoDB, DataArrayCRM);
-
-    // await console.log("afterArray: ", afterArray.length);
-
-    if(diffArray.length == 0){
-        await console.log("is in the CRM");
-    }else{
-        await console.log("add to CRM");
-        await diffArray.map(data => addCRM(data))
-    }
-}
 exports.handler = (event, context, callback) => {
-    foo();
+    
+    event.Records.forEach((record) => {
+        
+        if(record.dynamodb.NewImage == undefined){
+            console.log("Remove Lead");
+        }else{
+            
+        console.log("Start Insert");
+        crm.Contact.insert([{
+                name: record.dynamodb.NewImage.customName.S,
+                responsible_user_id: "3316417",
+                tags: "купить майнер",
+                custom_fields: [
+                    { 
+                        id: "543953",
+                        name: 'Телефон',
+                        code: 'PHONE',
+                        values: [{
+                            value: record.dynamodb.NewImage.phone.S,
+                            enum: 907777
+                        }],
+                        is_system: true 
+                    },
+                    { 
+                        id: "543955",
+                        name: 'Email',
+                        code: 'EMAIL',
+                        values: [{
+                            value: record.dynamodb.NewImage.email.S,
+                            enum: 907789
+                        }],
+                        is_system: true 
+                    }
+                ]
+            }])
+            .then(contact => {
+                console.log("Contanct ID", contact._response._embedded.items[0].id );
+                console.log(record.dynamodb.NewImage.TxAssetID.S, ": Add to Lead");
+                crm.Lead.insert([{
+                    name: 'Покупка майнеров с client`а',
+                    status_id: "29857402",
+                    responsible_user_id: "55014691",
+                    sale: 115,
+                    tags: "buy_miner",
+                    contacts_id: [
+                        contact._response._embedded.items[0].id 
+                    ],
+                    custom_fields: [
+                        {
+                            id: 657873,
+                            name: "Адрес доставки",
+                            values: [{ 
+                                value: record.dynamodb.NewImage.postCode.S + ', ' + record.dynamodb.NewImage.country.S + ', ' + record.dynamodb.NewImage.countryState.S + ', ' + record.dynamodb.NewImage.city.S + ', ' + record.dynamodb.NewImage.address.S, 
+                            }],
+                            is_system: false 
+                        },
+                        {
+                            id: 664225,
+                            name: "TxAssetID",
+                            values: [{ 
+                                value: record.dynamodb.NewImage.TxAssetID.S
+                            }],
+                            is_system: false
+                        },
+                        {
+                            id: 664227,
+                            name: "countMiners",
+                            values: [{ 
+                                value: record.dynamodb.NewImage.countMiners.N
+                            }],
+                            is_system: false
+                        },
+                        {
+                            id: 664381,
+                            name: "referal",
+                            values: [{ 
+                                value: record.dynamodb.NewImage.referal.S
+                            }],
+                            is_system: false
+                        },
+                    ]
+                }]);
+            });
+        console.log("Stop Insert");
+        
+        }
+
+    });
+
 }
